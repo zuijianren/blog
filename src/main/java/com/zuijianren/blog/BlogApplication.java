de8a9fceb5bc8d8a49c9fc8349fef5d1e891b95d
zuijianren <1460742681@qq.com>
2020-08-26 16:04:51 +0800

    简单修改，准备添加一点功能

--- src/main/java/com/zuijianren/blog/BlogApplication.java
+++ src/main/java/com/zuijianren/blog/BlogApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class BlogApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return super.configure(builder);
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
